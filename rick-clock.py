db5c321c6b559b3b5591648197bf9eefcdc11cc0
Rick G <rick030503@gmail.com>
2022-05-17 12:12:54 +0200

    17052022

---
+++ rick-clock.py
@@ -0,0 +1,17 @@
+# Create a digital clock
+
+
+# Librairies
+
+from tkinter import *
+import time
+
+# Code
+
+gui = Tk(className=' Digital Clock')
+gui.geometry("2000x1000") 
+
+gui.mainloop()
+ 
+
+# Launching

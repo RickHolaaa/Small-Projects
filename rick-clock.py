1dfe3a48e87e8800c1b1f67d9044187c15846897
Rick G <rick030503@gmail.com>
2022-05-18 22:01:04 +0200

    18052022

--- rick-clock.py
+++ rick-clock.py
@@ -5,22 +5,27 @@
 
 from tkinter import *
 import tkinter.ttk as ttk
-import time
+import datetime
+
 
 # Code
 
 gui = Tk(className=' Digital Clock')
 gui.geometry("2000x1000") 
 
-#label.place(x=200,y=100,anchor="center")
-#label.pack(expand=True)
+now = datetime.datetime.now()
 
 canvas= Canvas(gui, width= 2000, height= 1000, bg="SpringGreen2")
-canvas.create_text(700, 350, text="HELLO WORLD", fill="black", font=('Helvetica 15 bold'))
-canvas.pack()
+canvas.create_text(700, 350, text="Current date and time : ", fill="black", font=('Helvetica 50 bold'))
+tm = canvas.create_text(700,450,text=now.strftime("%Y-%m-%d %H:%M:%S"),fill="black",font=("Helvetica 35 bold"))
 
+def change_time():
+    now = datetime.datetime.now()
+    canvas.itemconfig(tm, text=now.strftime("%Y-%m-%d %H:%M:%S"))
+    canvas.pack()
+    canvas.after(100,change_time)
 
-
+change_time()
 gui.mainloop()
  
 
